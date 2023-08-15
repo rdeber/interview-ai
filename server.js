abcaa748d34ac1cc0affb95d8432e90075eb67fd
Ryan DeBerardinis <rdeber@gmail.com>
2023-08-14 20:52:20 -0400

    Get openapi working with gpt 3.5

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Test endpoint
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });
+
+// POST endpoint to forward data to OpenAI
+app.post('/api/ask', async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text is required." });
+    }
+
+    const requestBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: text
+        }
+      ],
+      temperature: 0.7
+    };
+
+    const openaiResponse = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json',
+      }
+    });
+
+    res.json({ response: openaiResponse.data.choices[0]?.message.content.trim() });
+
+  } catch (error) {
+    console.error('Error fetching from OpenAI:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to fetch response from OpenAI.' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
